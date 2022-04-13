18443843dc5a44baa26ff49f0b601a774b2aaaf0
AR-Shahin <mdshahinmije96@gmail.com>
2022-04-13 15:24:32 +0600

    first class

---
+++ template/resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin_master')
+@section('title', 'Dashboard Page')
+
+@section('master_content')
+
+@endsection
+
+@push('css')
+    <style>
+        body {
+            background: red !important;
+        }
+
+    </style>
+@endpush
+
+
+@push('js')
+    <script>
+
+    </script>
+@endpush

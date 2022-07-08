7842b60a28695a160fe464807ca75a75b98373bb
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-08 11:27:07 +0600

    show seller in agent

--- palash/resources/views/agent/dashboard.blade.php
+++ palash/resources/views/agent/dashboard.blade.php
@@ -8,5 +8,6 @@
 <div class="container mt-2">
     <h3>Name : {{ auth('agent')->user()->name ?? "" }}</h3>
     <h3>Email : {{ auth('agent')->user()->email ?? ""}}</h3>
+    <h3>Seller : {{ auth('agent')->user()->seller->name ?? ""}}</h3>
 </div>
 @stop
